01328c55633bd61ed803eb9899488967644c5e8f
Thomaz Rodrigues <thomazrodrigues668@gmail.com>
2024-05-14 22:37:34 -0300

    homepage

---
+++ src/components/Card.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  route: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  }
+});
+
+</script>
+
+<template>
+    <!-- <div class="card">
+        <div class="img">
+            <img src={{ props.image }} alt="Imagem do card">
+        </div>
+        <div class="card-header">
+            <h3>{{ props.title }}</h3>
+        </div>
+        <div class="card-body">
+            <router-link to={{ props.route }}></router-link>
+        </div>
+    </div> -->
+</template>
+
+<style scoped>
+    .card {
+        width: 300px;
+        height: 300px;
+        background-color: #fff;
+        border-radius: 20px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        margin: 20px;
+    }
+
+    .img {
+        width: 100%;
+        height: 150px;
+        border-radius: 20px 20px 0 0;
+        overflow: hidden;
+    }
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .card-header {
+        padding: 10px;
+        text-align: center;
+    }
+
+    .card-body {
+        padding: 10px;
+        text-align: center;
+    }
+</style>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,94 @@
 <script setup>
+import Card from "../components/Card.vue";
 
 </script>
 
 <template>
-  <main>
-    <h1>Teste</h1> 
-  </main>
+  <header class="header">
+    <div class="top">
+      <h1>Dashboard</h1>
+      <div>
+        <img class="fala" src="../assets/img/fala.png">
+      </div>
+    </div>
+  </header>
+  <section>
+      <div class="content">
+          <!-- <Card title="Matemática"  
+                route="/" 
+                image="../assets/img/curuja.png"
+          /> -->
+      </div>
+  </section>
+
+  <div class="nav">
+    <router-link class="link" to="/home">
+      <img src="../assets/img/pessoa.png" alt="Home">
+    </router-link>
+    <router-link class="link" to="/">
+      <img src="../assets/img/ouvir.png" alt="Home">
+    </router-link>
+    <router-link class="link" to="/">
+      <img src="../assets/img/casa.png" alt="Home">
+    </router-link>
+    <router-link class="link" to="/">
+      <img src="../assets/img/trofeu.png" alt="Home">
+    </router-link>
+    <router-link class="link" to="/">
+      <img src="../assets/img/conf.png" alt="Home">
+    </router-link>
+  </div>
 </template>
 
 <style scoped>
-  
+  .header {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    background-color: #3E96C8;
+    color: #fff;
+    padding: 20px;
+    border-radius: 0 0 20px 20px;
+  }
+
+  .top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 20px 0;
+    background-color: #3E96C8;
+    color: #fff;
+    
+  }
+
+  .fala {
+    width: 50px;
+  }
+
+  .content {
+    position: absolute;
+    top: 2rem;
+  }
+
+  .nav {
+    display: flex;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: #3E96C8;
+    padding: 20px;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .link {
+    display: inline-block;
+  }
+
+  .link img {
+    width: 50px;
+    margin: 0 10px;
+  }
 </style>
